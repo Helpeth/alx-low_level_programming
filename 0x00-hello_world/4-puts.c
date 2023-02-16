561a1df9963f5234ed170ac8f136f5c55a1df262
Helpeth <emmanuelkelechi343@gmail.com>
2023-02-16 13:34:54 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main - printing a program with put function
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
