3972eb519caed6dc3d44496a0dbaaa98a6be0299
Helpeth <emmanuelkelechi343@gmail.com>
2023-02-16 16:20:13 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <unistd.h>
+/**
+ * main - print c program a line to standard error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" -dora korpar, 2015-10-19\n", stdout);
+	return (1);
+}
