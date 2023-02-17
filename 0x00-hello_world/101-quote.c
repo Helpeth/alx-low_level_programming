88c8f03f844e351c1275b16e861042611ce1e515
Helpeth <emmanuelkelechi343@gmail.com>
2023-02-16 23:00:21 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,10 @@
  * main - print c program a line to standard error
  * Return: 1 (success)
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", +
+			stdout);
 	return (1);
 }
