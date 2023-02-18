6247fccb15c3d6390f88134a150654e27b0161fb
Helpeth <emmanuelkelechi343@gmail.com>
2023-02-18 15:24:07 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing decimal numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+	return (0);
+}
