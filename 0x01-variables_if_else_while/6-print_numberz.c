7d7530f2c9c850a5b13929de28c0475fac11cef3
Helpeth <emmanuelkelechi343@gmail.com>
2023-02-18 15:55:25 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing decimals numers only
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar((i % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
